10ce9bc7f2f8a7915e0bc3f94f0bff4ec294ac0f
Ignacio Arenas <ignacioarenas2010@hotmail.com>
2022-09-01 22:57:33 -0300

    Segundo desafío

---
+++ main.js
@@ -0,0 +1,9 @@
+let nombreAlumno = prompt("Ingrese su nombre: ");
+let notaPrimerParcial = parseInt(prompt("Ingrese la nota del primer parcial: "));
+let notaSegundoParcial =  parseInt(prompt("Ingrese la nota del segundo parcial: "));
+let promedioNotas = (notaPrimerParcial + notaSegundoParcial) / 2;
+
+alert ("Tu promedio es de " + promedioNotas);
+
+console.log ("El alumno " + nombreAlumno + "Primer nota: " + notaPrimerParcial + "Segunda nota: " +notaSegundoParcial);
+console.log("El promedio de notas del alumno es de: " + promedioNotas);
